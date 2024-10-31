bc6d428f158c948fa96f09d2908dd2c3c0225da8
Disha010206 <dishakeshava06@gmail.com>
2024-10-31 13:17:40 +0530

    car.py

--- car.py
+++ car.py
@@ -2,8 +2,14 @@ import cv2
 import numpy as np
 import serial
 import time
+import mediapipe as mp
+
 arduino = serial.Serial('COM3', 9600)
 time.sleep(2)
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
 
 def count_fingers(hand_landmarks):
     fingers = 0
@@ -13,7 +19,7 @@ def count_fingers(hand_landmarks):
         fingers += 1
     if hand_landmarks[12][1] < hand_landmarks[11][1]:
         fingers += 1
-    if hand_landmarks[16][1] < hand_landmarks[15][1]: 
+    if hand_landmarks[16][1] < hand_landmarks[15][1]:
         fingers += 1
     if hand_landmarks[20][1] < hand_landmarks[19][1]:
         fingers += 1
@@ -26,24 +32,34 @@ while True:
     if not ret:
         break
 
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(rgb_frame)
+
     hand_landmarks = []
+    if results.multi_hand_landmarks:
+        for hand_landmark in results.multi_hand_landmarks:
+            for id, lm in enumerate(hand_landmark.landmark):
+                h, w, c = frame.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                hand_landmarks.append((cx, cy))
+            mp_draw.draw_landmarks(frame, hand_landmark, mp_hands.HAND_CONNECTIONS)
+
     if hand_landmarks:
         fingers = count_fingers(hand_landmarks)
-
-        if fingers == 0:
+        if fingers == 1:
             arduino.write(b'F')
         elif fingers == 5:
             arduino.write(b'S')
-        elif fingers == 1:
-            arduino.write(b'H')
         elif fingers == 2:
-            arduino.write(b'R') 
-
-    
+            arduino.write(b'H')
+        elif fingers == 3:
+            arduino.write(b'R')
+        #print(fingers)
     cv2.imshow("Hand Gesture Control", frame)
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
+cv2.destroyAllWindows()
+
 cv2.destroyAllWindows()
